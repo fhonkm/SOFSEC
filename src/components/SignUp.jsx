88a21f41c2fa65cac1d2993178f25da2e59cfaab
miggyyboi <migsfrags@gmail.com>
2025-03-28 15:42:12 +0800

    email providers list added

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,28 +1,33 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
+import { allowedProviders } from '../constants/emailProviders';
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
   const [emailError, setEmailError] = useState('');
   const [password, setPassword] = useState("");  
-  const allowedProviders = [
-    "gmail.com", 
-    "yahoo.com", 
-    "outlook.com", 
-    "icloud.com", 
-    "protonmail.com", 
-    "aol.com", 
-    "hotmail.com"
-  ];
+  const [passwordError, setPasswordError] = useState('');
+
   const validCredentials = email && password;
   const { signUp } = UserAuth();
   const navigate = useNavigate();
 
+  const checkPassword = (pass) => {
+    // 8 min, 64 max, 1 uppercase, 1 number, 1 special
+    const passRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+[\]{};:'",.<>/?]).{8,64}$/;
+    
+    if (!passRegex.test(pass)) {
+      return "Password requirements: 8 minimum, 64 max, 1 uppercase, 1 number, 1 special. Please follow these requirements.";
+    }
+
+    return "";
+  };
+
   const validateEmail = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-      return 'Invalid email format.';
+      return "Invalid email format.";
     }
 
     const domain = email.split('@')[1];
@@ -32,16 +37,19 @@ export default function SignUp() {
       )}.`;
     }
 
-    return '';
+    return "";
   };
 
   const handleRegister = async (e) => {
     e.preventDefault();
 
     const emailValidationResult = validateEmail(email);
-    setEmailError(emailValidationResult)
+    setEmailError(emailValidationResult);
 
-    if (!emailValidationResult && validCredentials) {
+    const passwordValidationResult = checkPassword(password);
+    setPasswordError(passwordValidationResult);
+
+    if (!emailValidationResult && !passwordValidationResult && validCredentials) {
         // console.log("clicked on register");
 
         try {
@@ -59,6 +67,8 @@ export default function SignUp() {
 
     return (
         <div className="flex flex-col justify-center items-center h-screen">
+            <h1 className="mb-10">Email confirmation will be sent after registering. Please confirm your email to login.</h1>
+
             <div>
                 <form onSubmit={handleRegister} className="mb-4 space-y-4 w-60 flex flex-col justify-center text-center">
                   <input
@@ -80,6 +90,9 @@ export default function SignUp() {
                     onChange={(e) => setPassword(e.target.value)}
                     className="border rounded p-1"
                   />
+
+                  {passwordError && <p style={{ color: 'red' }}>{passwordError}</p>}
+
                     <div className="flex flex-col space-y-2"> 
                         <button type="submit" className={` bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${validCredentials ? "" : "cursor-not-allowed opacity-50"}`}>
                         Register
---
+++ src/constants/emailProviders.js
@@ -0,0 +1,9 @@
+export const allowedProviders = [
+    "gmail.com",
+    "yahoo.com",
+    "outlook.com",
+    "icloud.com",
+    "protonmail.com",
+    "aol.com",
+    "hotmail.com"
+  ];
